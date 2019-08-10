35bcb487a3c337a3401092ece0adae7c3fe98db6
Vlad Ciphersky <32297717+vladciphersky@users.noreply.github.com>
2019-08-10 23:31:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var request = require('request-promise-native');
 const hostname = "https://api.server-discord.com";
 
 function options(uri, token) {
-	if(!uri) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан адрес метода");
-	if(!token) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан API токен");
+    if(!uri) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан адрес метода");
+    if(!token) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан API токен");
+
     return {
         uri: hostname + uri,
         qs: {
@@ -33,16 +34,17 @@ module.exports = function (token) {
 	).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
 
 	this.userrated = (userID) => {
-        if(!userID) return console.error("❌ Ошибка в работе sdc-api | Недостаточно аргументов");
-        return request(
+		if(!userID) return console.error("❌ Ошибка в работе sdc-api | Недостаточно аргументов");
+		return request(
 			options(`/user/${userID}/rated`, token)
 		).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
 	};
 
 	this.warns = (userID) => {
 		if(!userID) return console.error("❌ Ошибка в работе sdc-api | Недостаточно аргументов");
-        return request(
+        	return request(
 			options(`/warns/${userID}`, token)
 		).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
-    };
+    	};
 };
+// end.
