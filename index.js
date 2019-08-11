66606cb9f6524beba8dd3d5ab7ccc81f26589ca2
Василий <vasiliy072000mail.ru@gmail.com>
2019-08-11 20:40:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
-// jshint esversion: 6
 var request = require('request-promise-native');
 const hostname = "https://api.server-discord.com";
 
-function options(uri, token) {
-    if(!uri) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан адрес метода");
-    if(!token) return console.error("❌ [OPTIONS] Ошибка в работе sdc-api | Не указан API токен");
-
-    return {
-        uri: hostname + uri,
-        qs: {
-            dKey: token
-        },
-        headers: {
-            'User-Agent': `NodeJS (sdc-api | ${uri} | ${token})`
-        },
-        json: true
-    };
-}
-
 module.exports = function (token) {
-	if(!token) return console.error("❌ Ошибка в работе sdc-api | Не указан API ключ");
+	if(!token) return console.error("[sdc-api] Ошибка аргументов | Не указан API ключ!");
+	
+	this.options = (uri, method = "GET") => {
+		if (!uri) return console.error("[sdc-api] Ошибка в работе модуля | Не указан адрес метода.");
+		
+		return {
+			method: method
+    	uri: hostname + uri,
+    	qs: {dKey: token},
+    	headers: {'User-Agent': `sdc-api/1.0.2 (${uri} | ${token})`},
+    	json: true
+  	};
+	};
 
-	this.guild = (guildID) => request(
-		options(guildID ? `/guild/${guildID}` : `/guild`, token)
-	).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
+	this.guild = (guildID) => request(this.options(guildID ? `/guild/${guildID}` : `/guild`))
+		.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 
-	this.guildplace = (guildID) => request(
-		options(guildID ? `/guild/${guildID}/place` : `/guild/place`, token)
-	).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
+	this.guildplace = (guildID) => request(this.options(guildID ? `/guild/${guildID}/place` : `/guild/place`))
+		.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 
-	this.guildrated = (guildID) => request(
-		options(guildID ? `/guild/${guildID}/rated` : `/guild/rated`, token)
-	).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
+	this.guildrated = (guildID) => request(this.options(guildID ? `/guild/${guildID}/rated` : `/guild/rated`))
+		.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 
 	this.userrated = (userID) => {
-		if(!userID) return console.error("❌ Ошибка в работе sdc-api | Недостаточно аргументов");
-		return request(
-			options(`/user/${userID}/rated`, token)
-		).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
+		if(!userID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID пользователя!");
+		
+		return request(this.options(`/user/${userID}/rated`))
+			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 	};
 
 	this.warns = (userID) => {
-		if(!userID) return console.error("❌ Ошибка в работе sdc-api | Недостаточно аргументов");
-        	return request(
-			options(`/warns/${userID}`, token)
-		).then((r) => r, (e) => console.error("❌ Ошибка в работе sdc-api | ", e));
-    	};
+		if(!userID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID пользователя!");
+    
+		return request(this.options(`/warns/${userID}`))
+			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
+  };
 };
-// end.
