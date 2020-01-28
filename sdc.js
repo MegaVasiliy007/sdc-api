3d9cb0533a4b8c686bb40ad681c7db9c2cd33400
vladciphersky <32297717+vladciphersky@users.noreply.github.com>
2020-01-28 15:25:50 +0500

    1.0.6
    • Авто-пост статистики при указании клиента бота
    • Маленькое изменение в функции для генерации настроек запроса

--- sdc.js
+++ sdc.js
@@ -1,7 +1,17 @@
+var { version } = require('./package');
 var request = require('request-promise-native');
-const hostname = "https://api.server-discord.com/v2";
 
-module.exports = function (token) {
+const paths = {
+	hostname: "https://api.server-discord.com/v2",
+	botsPath: "https://bots.server-discord.com"
+};
+
+const isSupported = (client) => {
+	if(client.guilds !== undefined || client.user !== undefined || client.users !== undefined) return true;
+	else return false;
+};
+
+module.exports = function (token, client) {
 	if(!token) return console.error("[sdc-api] Ошибка аргументов | Не указан API ключ!");
 	
 	this.options = (uri, method = "GET", args = null) => {
@@ -9,37 +19,57 @@ module.exports = function (token) {
 		
 		const optionsObj = {
 			method: method,
-    			uri: encodeURI(hostname + uri),
-    			headers: {
-				'User-Agent': `sdc-api/1.0.5 (${uri})`,
-				'Authorization': 'SDC '+token
+			uri: encodeURI(paths.hostname + uri),
+			headers: {
+				'User-Agent': `sdc-api/${version} (${uri})`,
+				'Authorization': 'SDC ' + token
 			},
-    			json: true
-  		};
+			json: true
+		};
 		
 		if(args !== null) optionsObj.form = args;
 		return optionsObj;
 	};
 
+	if(client) {
+		if(isSupported(client)) {
+			client.on('ready', () => {
+				request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
+					.then((r) => {
+						if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
+						else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
+					}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
+
+				setInterval(() => {
+					request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
+						.then((r) => {
+							if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
+							else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
+						}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
+				}, 900000);
+			});
+		} else return console.warn("[sdc-api | Авто-пост] Библиотека бота не поддерживается!");
+	}
+
 	this.guild = (guildID) => {
 		if(!guildID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID сервера!");
 		
 		return request(this.options(`/guild/${guildID}`))
-			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e))
+			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 	};
 	
 	this.guildplace = (guildID) => {
 		if(!guildID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID сервера!");
 		
 		return request(this.options(`/guild/${guildID}/place`))
-			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e))
+			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 	};
 	
 	this.guildrated = (guildID) => {
 		if(!guildID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID сервера!");
 		
 		return request(this.options(`/guild/${guildID}/rated`))
-			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e))
+			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 	};
 
 	this.userrated = (userID) => {
@@ -53,13 +83,6 @@ module.exports = function (token) {
 		if(!userID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID пользователя!");
     
 		return request(this.options(`/warns/${userID}`))
-			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
-	};
-	
-	this.botStats = (botID, args = { servers: 0, shards: 0 }) => {
-		if(!botID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID бота!");
-		
-		return request(this.options(`/bots/${botID}/stats`, 'POST', args))
 			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
 	};
 };
