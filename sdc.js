88df143c33df149c23713d3994bda48f679a147f
vladciphersky <32297717+vladciphersky@users.noreply.github.com>
2020-01-29 09:11:59 +0500

    Update sdc.js

--- sdc.js
+++ sdc.js
@@ -3,15 +3,20 @@ var request = require('request-promise-native');
 
 const paths = {
 	hostname: "https://api.server-discord.com/v2",
-	botsPath: "https://bots.server-discord.com"
+	botsPath: "https://bots.server-discord.com",
+	github: "https://github.com/MegaVasiliy007/sdc-api"
 };
 
 const isSupported = (client) => {
-	if(client.guilds !== undefined || client.user !== undefined || client.users !== undefined) return true;
+	if(
+		client.options
+		&& client.options.ws
+		&& client.options.ws.properties['$device'] == 'discord.js'
+	) return true;
 	else return false;
 };
 
-module.exports = function (token, client) {
+module.exports = function (token) {
 	if(!token) return console.error("[sdc-api] Ошибка аргументов | Не указан API ключ!");
 	
 	this.options = (uri, method = "GET", args = null) => {
@@ -30,26 +35,6 @@ module.exports = function (token, client) {
 		if(args !== null) optionsObj.form = args;
 		return optionsObj;
 	};
-
-	if(client) {
-		if(isSupported(client)) {
-			client.on('ready', () => {
-				request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
-					.then((r) => {
-						if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
-						else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
-					}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
-
-				setInterval(() => {
-					request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
-						.then((r) => {
-							if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
-							else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
-						}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
-				}, 900000);
-			});
-		} else return console.warn("[sdc-api | Авто-пост] Библиотека бота не поддерживается!");
-	}
 
 	this.guild = (guildID) => {
 		if(!guildID) return console.error("[sdc-api] Ошибка аргументов | Не указан ID сервера!");
@@ -84,5 +69,25 @@ module.exports = function (token, client) {
     
 		return request(this.options(`/warns/${userID}`))
 			.then((r) => r, (e) => console.error("[sdc-api] Ошибка в работе | ", e));
+	};
+
+	this.setAutopost = (client, interval = 900000) => {
+		if(!client) return console.error("[sdc-api] Ошибка аргументов | Не указан клиент бота!");
+		if(!isSupported(client)) return console.error('[sdc-api] Ошибка аргументов | Библиотека бота не поддерживается! Пожалуйста, сообщите нам на GitHub:\n' + encodeURI(`${paths.github}/issues`));
+		
+		if(interval && interval <= 15000) return console.error("[sdc-api] Ошибка аргументов | Отправка статистики возможна не менее одного раза в 15 секунд!");
+		request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
+			.then((r) => {
+				if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
+				else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
+			}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
+
+		return setInterval(() => {
+			request(this.options(`/bots/${client.user.id}/stats`, 'POST', { servers: client.guilds.size, shards: ((client.shards == undefined) ? 0 : client.shards.size) }))
+				.then((r) => {
+					if(r.error) return console.error("[sdc-api | Авто-пост] Ошибка в работе\n" + r.error.message);
+					else return console.info("[sdc-api | Авто-пост] Статистика для " + client.user.tag + " опубликована на мониторинг.\n" + encodeURI(paths.botsPath + "/" + client.user.id));
+				}, (e) => console.error("[sdc-api | Авто-пост] Ошибка в работе | ", e));
+		}, interval);
 	};
 };
