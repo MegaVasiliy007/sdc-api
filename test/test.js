587291b5bea1b17c4e498629152ef5792712de1e
Alexander Herasymov <drgerasimova8@gmail.com>
2020-08-11 14:53:06 +0300

    let -> const

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ function message(type, argone, argtwo, argthree) {
   console.log("============================================");
 }
 
-let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU2MzIwMzA3Mjk5MDY0MjE4NiIsInBlcm1zIjowLCJpYXQiOjE1NzcxMjE4NDZ9.Y5qSkDQhOLsLbE6tcyp9e4ua0FtCrN1ykBBe0rJ9TXo";
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU2MzIwMzA3Mjk5MDY0MjE4NiIsInBlcm1zIjowLCJpYXQiOjE1NzcxMjE4NDZ9.Y5qSkDQhOLsLbE6tcyp9e4ua0FtCrN1ykBBe0rJ9TXo";
 // API ключ можно получить на странице редактирования вашего бота
 
 const SDC = require("../sdc");
